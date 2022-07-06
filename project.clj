c0caa858aceb5379dff71e31926fe01c1b298a00
Andrey Krainyak <mode.andrew@gmail.com>
2022-07-06 14:54:38 +0800

    Enable coverage reports

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :dependencies [[criterium "0.4.6"]
                  [org.clojure/clojure "1.11.1"]
                  [org.clojure/test.check "1.1.1"]]
+  :plugins [[lein-cloverage "1.2.4"]]
   :repl-options {:init-ns hashmap.core})
